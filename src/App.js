204d1777efb8963fc7d533db95a1d00b43d897cb
doronma <marcus_doron@yahoo.com>
2022-10-29 12:44:07 +0300

    add banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './Banner'
+import Banner from './Banner';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello World
+        <Banner/>
       </header>
     </div>
   );
---
+++ src/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>Hello World Banner</div>
+  )
+}
+
+export default Banner
