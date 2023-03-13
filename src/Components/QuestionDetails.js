68b51406d0330eb663c2d9586c7570672ffcaf46
doronma <marcus_doron@yahoo.com>
2023-03-13 19:36:28 +0200

    add answerform

--- src/Components/QuestionDetails.js
+++ src/Components/QuestionDetails.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import { useContext, useState, useEffect } from 'react';
+import { AccountContext } from './Account';
 import { useNavigate, useParams } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
@@ -10,10 +13,11 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { getUuid, getCurrentTime } from "../utils/formUtils"
 
 const QuestionDetails = () => {
 
+    const { getUserName } = useContext(AccountContext);
     let { id } = useParams();
     const navigate = useNavigate()
 
@@ -25,6 +29,7 @@ const QuestionDetails = () => {
     const url_answeres = "https://pwqmfe6648.execute-api.eu-central-1.amazonaws.com/dev/questions/" + id + "/answers"
     const [fetchedData, setFetchedData] = useState([]);
     const [fetchedAnsweres, setFetchedAnsweres] = useState([]);
+    const [showCreateAnswer, setShowCreateAnswer] = useState(false)
     useEffect(() => {
         const getData = async () => {
             const result = await axios.get(url)
@@ -42,6 +47,16 @@ const QuestionDetails = () => {
         getAnsweres();
     }, []);
 
+    const cardActions = () => {
+        if (!showCreateAnswer) {
+            return (
+                <CardActions>
+                    <Button size="small" onClick={addAnswer}>Create Answer</Button>
+                </CardActions>
+
+            )
+        }
+    }
     const questions = () => {
         if (fetchedData.data) {
             let questionArray = fetchedData.data
@@ -69,6 +84,7 @@ const QuestionDetails = () => {
                                 {question.message}
                             </Typography>
                         </CardContent>
+                        {cardActions()}
                     </Card>
                 </Grid>
             ))
@@ -76,8 +92,95 @@ const QuestionDetails = () => {
         }
     }
 
+    const addAnswer = () => {
+        setShowCreateAnswer(true)
+
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const addAnswerForm = () => {
+        if (showCreateAnswer) {
+            return (
+                <div>
+                    <Container component="main" maxWidth="xs">
+                        <CssBaseline />
+                        <Box
+                            sx={{
+                                marginTop: 8,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Typography component="h1" variant="h5">
+                                Create Answer
+                            </Typography>
+                            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="id"
+                                    label="ID"
+                                    name="id"
+                                    value={getUuid()}
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="user"
+                                    label="User"
+                                    name="user"
+                                    value={getUserName()}
+                                />
+
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="answer"
+                                    name="answer"
+                                    label="answer"
+                                    multiline
+                                    rows={4}
+                                    autoFocus
+                                />
+
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                >
+                                    Send
+                                </Button>
+                            </Box>
+                        </Box>
+                    </Container>
+                </div>
+            )
+        }
+    }
+
+    const answertTitle = () => {
+        if (!showCreateAnswer && fetchedAnsweres.data && fetchedAnsweres.data.length > 0) {
+            return (
+                <Container maxWidth="sm">
+                    <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                        Answers
+                    </Typography>
+                </Container>
+            )
+        }
+    }
+
     const answers = () => {
-        if (fetchedAnsweres.data) {
+        if (fetchedAnsweres.data && !showCreateAnswer) {
+
             let answerArray = fetchedAnsweres.data
             console.log(answerArray)
             return answerArray.map((answer) => (
@@ -103,6 +206,23 @@ const QuestionDetails = () => {
                 </Grid>
             ))
 
+        }
+    }
+
+    const backToQuestionsButton = () => {
+        if (!showCreateAnswer) {
+            return (
+                <Container maxWidth="sm">
+                    <Stack
+                        sx={{ pt: 4 }}
+                        direction="row"
+                        spacing={2}
+                        justifyContent="center"
+                    >
+                        <Button variant="contained" onClick={backToQuestions}>Back to Questions</Button>
+                    </Stack>
+                </Container>
+            )
         }
     }
 
@@ -136,27 +256,15 @@ const QuestionDetails = () => {
                         {questions()}
                     </Grid>
                 </Container>
-                <Container maxWidth="sm">
-                    <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                        Answers
-                    </Typography>
-                </Container>
+                {answertTitle()}
                 <Container sx={{ py: 1 }} maxWidth="md">
                     {/* End hero unit */}
                     <Grid container spacing={4}>
                         {answers()}
                     </Grid>
                 </Container>
-                <Container maxWidth="sm">
-                    <Stack
-                        sx={{ pt: 4 }}
-                        direction="row"
-                        spacing={2}
-                        justifyContent="center"
-                    >
-                        <Button variant="contained" onClick={backToQuestions}>Back to Questions</Button>
-                    </Stack>
-                </Container>
+                {backToQuestionsButton()}
+                {addAnswerForm()}
             </main>
         </div>
     );
