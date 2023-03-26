88d81e53dfdb31cc7168c21769e8343ac12756ab
doronma <marcus_doron@yahoo.com>
2023-03-26 20:49:45 +0300

    some cleanup and documentation

--- src/utils/formUtils.js
+++ src/utils/formUtils.js
@@ -12,25 +12,12 @@ const getCurrentTime = () => {
     return dateTime;
 }
 
-const parseDate = (dateToConvert) => {
-
-    let regExpConstructor = new RegExp("\\d*", 'g')
-    let day = Number(dateToConvert.match(regExpConstructor)[0])
-
-    regExpConstructor = new RegExp("-\\d*", 'g')
-    let result = dateToConvert.match(regExpConstructor);
-    let month = Number(result[0].substring(1))-1
-    let year = Number(result[1].substring(1))
-
-    regExpConstructor = new RegExp("\\s\\d*", 'g')
-    let hour = Number(dateToConvert.match(regExpConstructor)[0])
-
-    regExpConstructor = new RegExp(":\\d*", 'g')
-    result = dateToConvert.match(regExpConstructor)
-    let minute = Number(result[0].substring(1))
-    let seconds = Number(result[1].substring(1))
-
-    return Date.parse(new Date(year, month, day, hour, minute, seconds))
-}
+// courtesy of Bing chat
+function parseDate(string) {
+    const [date, time] = string.split(' ');
+    const [day, month, year] = date.split('-');
+    const [hours, minutes, seconds] = time.split(':');
+    return new Date(year, month - 1, day, hours, minutes, seconds);
+  }
 
 export { getUuid, getCurrentTime,parseDate }
